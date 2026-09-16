a6e7be033c8c37d5fa6b9e09c8039959a9e85dbf
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2026-09-16 04:27:56 +0300

    add core

---
+++ lib/core/resources/appcolors.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+
+abstract class Appcolors {
+  static Color Primary = const Color(0xff004182);
+  
+}
---
+++ lib/core/resources/assets-manager.dart
@@ -0,0 +1,4 @@
+
+abstract class AppAssets{
+  static const String routeLogo = "lib/core/resources/assets-manager.dart";
+}
